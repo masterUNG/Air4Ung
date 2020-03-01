35f4ac4158234a08220e0603295ce6c3a1f33cc8
masterUNG <phrombutr@gmail.com>
2020-03-01 10:18:38 +0700

    Design UI register Success

--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -11,13 +11,97 @@ class _RegisterState extends State<Register> {
 
   // Method
   Widget nameForm() {
-    return Row(mainAxisAlignment: MainAxisAlignment.center,
+    Color color = Colors.purple[600];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Container(
           width: 250.0,
-          child: TextField(),
+          child: TextField(
+            decoration: InputDecoration(
+              enabledBorder:
+                  UnderlineInputBorder(borderSide: BorderSide(color: color)),
+              helperStyle: TextStyle(color: color),
+              labelStyle: TextStyle(color: color),
+              icon: Icon(
+                Icons.supervised_user_circle,
+                size: 36.0,
+                color: color,
+              ),
+              helperText: 'Type Your Name in the Blank',
+              labelText: 'Display Name :',
+            ),
+          ),
         ),
       ],
+    );
+  }
+
+  Widget emailForm() {
+    Color color = Colors.green[700];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 250.0,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              enabledBorder:
+                  UnderlineInputBorder(borderSide: BorderSide(color: color)),
+              helperStyle: TextStyle(color: color),
+              labelStyle: TextStyle(color: color),
+              icon: Icon(
+                Icons.email,
+                size: 36.0,
+                color: color,
+              ),
+              helperText: 'Type Your Email in the Blank',
+              labelText: 'Email :',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget passwordForm() {
+    Color color = Colors.pink[600];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 250.0,
+          child: TextField(
+            decoration: InputDecoration(
+              enabledBorder:
+                  UnderlineInputBorder(borderSide: BorderSide(color: color)),
+              helperStyle: TextStyle(color: color),
+              labelStyle: TextStyle(color: color),
+              icon: Icon(
+                Icons.lock_open,
+                size: 36.0,
+                color: color,
+              ),
+              helperText: 'Type Your Password in the Blank',
+              labelText: 'Password :',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget registerButton() {
+    return Container(
+      margin: EdgeInsets.only(top: 20.0),
+      width: 250.0,
+      child: OutlineButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        borderSide: BorderSide(color: MyStyle().darkColor),
+        child: Text('Register', style: TextStyle(color: MyStyle().darkColor),),
+        onPressed: () {},
+      ),
     );
   }
 
@@ -28,10 +112,18 @@ class _RegisterState extends State<Register> {
         title: Text('Register'),
         backgroundColor: MyStyle().darkColor,
       ),
-      body: Column(
-        children: <Widget>[
-          nameForm(),
-        ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 20.0),
+          child: Column(
+            children: <Widget>[
+              nameForm(),
+              emailForm(),
+              passwordForm(),
+              registerButton(),
+            ],
+          ),
+        ),
       ),
     );
   }
